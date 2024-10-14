ab7e3e5435b3f720ab28639aa9966203cba28a95
Mikaela <mikaelacduarte@gmail.com>
2024-10-14 14:51:58 -0300

    Novos comentários

--- projeto-IFRN/logicentro/empresa/urls.py
+++ projeto-IFRN/logicentro/empresa/urls.py
@@ -3,9 +3,14 @@ from rest_framework.routers import DefaultRouter
 
 from empresa.views import EmpresaViewSet
 
+# Cria um roteador para definir as URLs da API
 router = DefaultRouter()
+
+# Registra o conjunto de visualizações para as empresas no roteador
 router.register(r'empresas', EmpresaViewSet)
 
+# Define as URLs da aplicação
 urlpatterns = [
+    # Inclui todas as URLs geradas pelo roteador
     path('', include(router.urls)),
 ]
--- projeto-IFRN/logicentro/usuario/urls.py
+++ projeto-IFRN/logicentro/usuario/urls.py
@@ -2,10 +2,12 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UsuarioViewSet
 
+# Cria um roteador para definir as URLs da API
 router = DefaultRouter()
 router.register(r'usuarios', UsuarioViewSet, basename='usuario')
 
+# Define as URLs da aplicação
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-token-auth/', include('rest_framework.urls')),
+    path('', include(router.urls)),  # Inclui as URLs geradas pelo roteador
+    path('api-token-auth/', include('rest_framework.urls')),  # Inclui a URL para autenticação
 ]
